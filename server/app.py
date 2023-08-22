0df66c408fe7a318545396709a8d2f656e85bf83
Maya Ashkinazi <maya01911@gmail.com>
2023-08-22 21:41:58 +0300

    added github actions

--- server/app.py
+++ server/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template,request,redirect,url_for
 from pymongo import MongoClient
 from bson.objectid import ObjectId
+from bson.errors import InvalidId
 import os
 from dotenv import load_dotenv
 
