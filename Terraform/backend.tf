c407bcb874fa0d20b7002408b437fe46c34c837a
Maya Ashkinazi <maya01911@gmail.com>
2023-08-30 13:53:16 +0300

    Updated cd.yml workflow

--- Terraform/backend.tf
+++ Terraform/backend.tf
@@ -3,6 +3,5 @@ terraform {
     bucket         = "test-terraform-state-todo"
     key            = "eks/terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "terraform-locks"
   }
 }
