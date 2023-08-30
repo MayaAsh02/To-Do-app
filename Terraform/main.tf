4d989864baf1750d327010c695f04f0684a71dde
Maya Ashkinazi <maya01911@gmail.com>
2023-08-30 14:18:28 +0300

    Updated cd.yml workflow

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -319,14 +319,16 @@ module "dns" {
 
 //install argocd on the cluster
 resource "helm_release" "argocd" {
-  name       = "argocd2"
+  name       = "argocd"
   repository = "https://argoproj.github.io/argo-helm"
   chart      = "argo-cd"
-  namespace  = "argocd2"
+  namespace  = "argocd"
   create_namespace = true
 
   set {
     name  = "server.service.type"
     value = "LoadBalancer"
   }
+
+  reset_values = true
 }
