46056832360bd3ee2d4d146b665a6b566e1a8a34
Maya Ashkinazi <maya01911@gmail.com>
2023-08-31 02:55:41 +0300

    try number 343892748

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -321,7 +321,7 @@ resource "helm_release" "argocd" {
   name       = "argocd"
   repository = "https://argoproj.github.io/argo-helm"
   chart      = "argo-cd"
-  namespace  = "argocd2"
+  namespace  = "argocd"
   create_namespace = true
 
   set {
