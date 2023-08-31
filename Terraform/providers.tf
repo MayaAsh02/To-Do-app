e97a26b494e3121d80e110997541be83fb23b423
Maya Ashkinazi <maya01911@gmail.com>
2023-08-31 17:28:36 +0300

    added something

--- Terraform/providers.tf
+++ Terraform/providers.tf
@@ -33,14 +33,9 @@ provider "aws" {
 }
 
 provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-  token                  = try(data.aws_eks_cluster_auth.cluster.token)
-    exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    command     = "aws"
-    args = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
-    }
+  host                   = module.eks[0].cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks[0].cluster_certificate_authority_data) 
+  token                  = try(data.aws_eks_cluster_auth.cluster[0].token, "")
 }
 
 provider "helm" {
