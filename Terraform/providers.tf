96641fc24de8007f01431a4653c6c0945231c087
Maya Ashkinazi <maya01911@gmail.com>
2023-08-31 17:42:34 +0300

    add number 54564565151

--- Terraform/providers.tf
+++ Terraform/providers.tf
@@ -27,10 +27,16 @@ provider "aws" {
   region = "us-east-1"
 }
 
+data "aws_eks_cluster_auth" "cluster" {
+  count      = var.create_eks_cluster ? 1 : 0
+  name       = module.eks[0].cluster_name
+  depends_on = [module.eks[0].cluster_arn]
+}
+
 provider "kubernetes" {
-  host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data) 
-  token                  = try(data.aws_eks_cluster_auth.cluster.token, "")
+  host                   = module.eks[0].cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks[0].cluster_certificate_authority_data) 
+  token                  = try(data.aws_eks_cluster_auth.cluster[0].token, "")
 }
 
 provider "helm" {
@@ -39,10 +45,4 @@ provider "helm" {
     cluster_ca_certificate = base64decode(module.eks[0].cluster_certificate_authority_data)
     token                  = try(data.aws_eks_cluster_auth.cluster[0].token, "")
   }
-}
-
-data "aws_eks_cluster_auth" "cluster" {
-  count      = var.create_eks_cluster ? 1 : 0
-  name       = module.eks.cluster_name
-  depends_on = [module.eks.cluster_arn]
 }
