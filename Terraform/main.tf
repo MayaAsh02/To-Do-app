1fc2027a641c82cb0da0ef84ba0c609465f665a7
Maya Ashkinazi <maya01911@gmail.com>
2023-08-31 17:12:33 +0300

    added something

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -43,33 +43,30 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "19.16.0"
 
+  count = var.create_eks_cluster ? 1 : 0
+
   cluster_name                   = local.name
   cluster_version                = local.cluster_version
   cluster_endpoint_public_access = true
 
   cluster_addons = {
-    coredns = {
-      version = "latest"
-      most_recent = true
-    }
     aws-ebs-csi-driver = {
-      version = "latest"
       most_recent = true
     }
-
+    coredns = {
+      most_recent = true
+    }
     kube-proxy = {
-      version = "latest"
       most_recent = true
     }
     vpc-cni = {
-      version = "latest"
       most_recent    = true
       before_compute = true
     }
   }
 
   vpc_id                   = module.vpc.vpc_id
-  subnet_ids               = module.vpc.public_subnets
+  subnet_ids               = module.vpc.private_subnets
   control_plane_subnet_ids = module.vpc.intra_subnets
 
   manage_aws_auth_configmap = true
@@ -77,44 +74,97 @@ module "eks" {
   eks_managed_node_group_defaults = {
     ami_type       = "AL2_x86_64"
     instance_types = ["t3.medium"]
+
+    # We are using the IRSA created below for permissions
+    # However, we have to deploy with the policy attached FIRST (when creating a fresh cluster)
+    # and then turn this off after the cluster/node group is created. Without this initial policy,
+    # the VPC CNI fails to assign IPs and nodes cannot join the cluster
+    # See https://github.com/aws/containers-roadmap/issues/1666 for more context
     iam_role_attach_cni_policy = true
   }
-  
+
   eks_managed_node_groups = {
-    default_node_group = {
-      use_custom_launch_template = false
-      disk_size = 50
-      remote_access = {
-        ec2_ssh_key               = module.key_pair.key_pair_name
-        source_security_group_ids = [aws_security_group.remote_access.id]
-      }
-    }
-
-    bottlerocket_default = {
-      use_custom_launch_template = false
-      ami_type = "BOTTLEROCKET_x86_64"
-      platform = "bottlerocket"
-    }
-
-    bottlerocket_add = {
-      ami_type = "BOTTLEROCKET_x86_64"
-      platform = "bottlerocket"
-
-      bootstrap_extra_args = <<-EOT
-        # extra args added
-        [settings.kernel]
-        lockdown = "integrity"
-      EOT
-    }
-
-    custom_ami = {
-      ami_type = "AL2_ARM_64"
-      ami_id = data.aws_ami.eks_default_arm.image_id
-      enable_bootstrap_user_data = true
-      instance_types = ["t4g.medium"]
-    }
-
-
+    # Default node group - as provided by AWS EKS
+    #    default_node_group = {
+    #      # By default, the module creates a launch template to ensure tags are propagated to instances, etc.,
+    #      # so we need to disable it to use the default template provided by the AWS EKS managed node group service
+    #      use_custom_launch_template = false
+    #
+    #      disk_size = 50
+    #
+    #      # Remote access cannot be specified with a launch template
+    #    }
+    #
+    #    # Default node group - as provided by AWS EKS using Bottlerocket
+    #    bottlerocket_default = {
+    #      # By default, the module creates a launch template to ensure tags are propagated to instances, etc.,
+    #      # so we need to disable it to use the default template provided by the AWS EKS managed node group service
+    #      use_custom_launch_template = false
+    #
+    #      ami_type = "BOTTLEROCKET_x86_64"
+    #      platform = "bottlerocket"
+    #    }
+    #
+    #    # Adds to the AWS provided user data
+    #    bottlerocket_add = {
+    #      ami_type = "BOTTLEROCKET_x86_64"
+    #      platform = "bottlerocket"
+    #
+    #      # This will get added to what AWS provides
+    #      bootstrap_extra_args = <<-EOT
+    #        # extra args added
+    #        [settings.kernel]
+    #        lockdown = "integrity"
+    #      EOT
+    #    }
+    #
+    #    # Custom AMI, using module provided bootstrap data
+    #    bottlerocket_custom = {
+    #      # Current bottlerocket AMI
+    #      ami_id   = data.aws_ami.eks_default_bottlerocket.image_id
+    #      platform = "bottlerocket"
+    #
+    #      # Use module user data template to bootstrap
+    #      enable_bootstrap_user_data = true
+    #      # This will get added to the template
+    #      bootstrap_extra_args = <<-EOT
+    #        # The admin host container provides SSH access and runs with "superpowers".
+    #        # It is disabled by default, but can be disabled explicitly.
+    #        [settings.host-containers.admin]
+    #        enabled = false
+    #
+    #        # The control host container provides out-of-band access via SSM.
+    #        # It is enabled by default, and can be disabled if you do not expect to use SSM.
+    #        # This could leave you with no way to access the API and change settings on an existing node!
+    #        [settings.host-containers.control]
+    #        enabled = true
+    #
+    #        # extra args added
+    #        [settings.kernel]
+    #        lockdown = "integrity"
+    #
+    #        [settings.kubernetes.node-labels]
+    #        project = var.project_name
+    #
+    #      EOT
+    #    }
+    #
+    #    # Use a custom AMI
+    #    custom_ami = {
+    #      ami_type = "AL2_ARM_64"
+    #      # Current default AMI used by managed node groups - pseudo "custom"
+    #      ami_id = data.aws_ami.eks_default_arm.image_id
+    #
+    #      # This will ensure the bootstrap user data is used to join the node
+    #      # By default, EKS managed node groups will not append bootstrap script;
+    #      # this adds it back in using the default template provided by the module
+    #      # Note: this assumes the AMI provided is an EKS optimized AMI derivative
+    #      enable_bootstrap_user_data = true
+    #
+    #      instance_types = ["t4g.medium"]
+    #    }
+    #
+    # Complete
     complete = {
       name            = "${var.environment}-${var.project_name}-eks"
       use_name_prefix = true
@@ -135,10 +185,8 @@ module "eks" {
         project = var.project_name
       }
 
-      associate_public_ip_address = true 
-
       update_config = {
-        max_unavailable_percentage = 33
+        max_unavailable_percentage = 33 # or set `max_unavailable`
       }
 
       description = "eks managed node group for ${var.project_name}"
@@ -189,6 +237,7 @@ module "eks" {
       }
     }
   }
+
   tags = local.tags
 }
 
