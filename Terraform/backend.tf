558e7168ac06234ba8d421715f31b520da3ea0a8
Maya Ashkinazi <maya01911@gmail.com>
2023-09-02 01:47:46 +0300

    Remove Terraform configuration files

--- Terraform/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "test-terraform-state-todo"
-    key            = "eks/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform_state"
-  }
-}
